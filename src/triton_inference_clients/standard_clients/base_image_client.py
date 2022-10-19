952165278479408e2403570d0bb4062a4a8bb019
Rishik Mourya <braindotai@gmail.com>
2022-10-19 12:40:01 +0530

    fixed triton issues

--- src/triton_inference_clients/standard_clients/base_image_client.py
+++ src/triton_inference_clients/standard_clients/base_image_client.py
@@ -4,9 +4,10 @@ from .base_client import BaseGRPCClient
 
 
 class BaseImageGRPCClient(BaseGRPCClient):
-    def generate_triton_request(self, inputs, *input_batches):
+    def triton_generate_request(self, inputs, *input_batches):
         joined_encodings = []
         split_indices = []
+        inference_params = self.inference_params.copy()
 
         for cv2_image in input_batches[0]:
             encodings = cv2.imencode('.jpg', cv2_image, [int(cv2.IMWRITE_JPEG_QUALITY), self.encoding_quality])[1]
@@ -14,10 +15,13 @@ class BaseImageGRPCClient(BaseGRPCClient):
 
             split_indices.append((split_indices[-1] if len(split_indices) else 0) + len(encodings))
         
-        self.inference_params['joined_encodings'] = np.expand_dims(np.concatenate(joined_encodings, axis = 0), 0)
-        self.inference_params['split_indices'] = np.expand_dims(np.array(split_indices), 0)
+        # self.inference_params['joined_encodings'] = np.expand_dims(np.concatenate(joined_encodings, axis = 0), 0) 
+        # self.inference_params['split_indices'] = np.expand_dims(np.array(split_indices), 0)
+        
+        inference_params['joined_encodings'] = np.expand_dims(np.concatenate(joined_encodings, axis = 0), 0) 
+        inference_params['split_indices'] = np.expand_dims(np.array(split_indices), 0)
 
-        return len(input_batches[0])
+        return len(input_batches[0]), inference_params
 
     
     def monolythic_preprocess(self, input_batch, resize_dim):
